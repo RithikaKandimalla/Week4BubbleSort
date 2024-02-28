3ade1da85b88613e8cebbdd940312660f94a35bb
Rithika Kandimalla <rithikak@usf.edu>
2024-02-28 14:43:25 -0500

    V1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿ 
+﻿/* Anuthor :Rithika Kandimalla
+  Date : 02/28/2024
+ This is Bubble Sort Algorithm */
+
 int[] bubble_sort_numbers = {  3, 62, 5, 16, 4, 10};
  
 Console.WriteLine("Here is the original array that will be used to execute the Bubble Sort Algorithm: ");
@@ -27,7 +30,7 @@ static void Bubble_Sort(int[] bs_arr)
             // If the next element value is less, then conduct the swap
             if (bs_arr[x] > bs_arr[x + 1])
             {
-                // Here you will swap temp and bs_arr[x]
+                // Here we will swap temp and bs_arr[x]
                 // The temp variable is used to temporarily hold the value of the current element so that it can be swapped
                 int temp = bs_arr[x];
                 bs_arr[x] = bs_arr[x + 1];
